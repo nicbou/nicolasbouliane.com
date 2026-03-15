362a2110c6d409a77b2757c93adaaf85819c4d18
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-03-15 19:05:56 +0100

    ursus_config: Linting

--- frontend/ursus_config.py
+++ frontend/ursus_config.py
@@ -4,58 +4,58 @@ from ursus.config import config
 import logging
 import os
 
-config.content_path = Path(__file__).parent / 'content'
-config.templates_path = Path(__file__).parent / 'templates'
-config.output_path = Path(__file__).parent.parent / 'output'
+config.content_path = Path(__file__).parent / "content"
+config.templates_path = Path(__file__).parent / "templates"
+config.output_path = Path(__file__).parent.parent / "output"
 
-config.site_url = os.environ.get('site_url', '')
-config.html_url_extension = ''
+config.site_url = os.environ.get("site_url", "")
+config.html_url_extension = ""
 config.minify_js = True
 config.minify_css = True
 
-config.context_processors.append('ursus.context_processors.git_date.GitDateProcessor')
+config.context_processors.append("ursus.context_processors.git_date.GitDateProcessor")
 
 config.context_globals = {
-    'now': datetime.now(),
-    'site_url': 'https://nicolasbouliane.com',
-    'is_golden': lambda uri, entry: 'golden' in entry.get('categories', []),
-    'is_not_golden': lambda uri, entry: 'golden' not in entry.get('categories', []),
+    "now": datetime.now(),
+    "site_url": "https://nicolasbouliane.com",
+    "is_golden": lambda uri, entry: "golden" in entry.get("categories", []),
+    "is_not_golden": lambda uri, entry: "golden" not in entry.get("categories", []),
 }
 
 config.image_transforms = {
-    '': {
-        'include': ('files/*.pdf', ),
-        'output_types': ('original', ),
+    "": {
+        "include": ("files/*.pdf",),
+        "output_types": ("original",),
     },
-    'content2x': {
-        'exclude': ('*.pdf', '*.svg'),
-        'max_size': (1848, 1848),
-        'output_types': ('original', 'webp'),
+    "content2x": {
+        "exclude": ("*.pdf", "*.svg"),
+        "max_size": (1848, 1848),
+        "output_types": ("original", "webp"),
     },
-    'content1x': {
-        'exclude': ('*.pdf', '*.svg'),
-        'max_size': (924, 924),
-        'output_types': ('original', 'webp'),
+    "content1x": {
+        "exclude": ("*.pdf", "*.svg"),
+        "max_size": (924, 924),
+        "output_types": ("original", "webp"),
     },
-    'content0.75x': {
-        'exclude': ('*.pdf', '*.svg'),
-        'max_size': (690, 690),
-        'output_types': ('original', 'webp'),
+    "content0.75x": {
+        "exclude": ("*.pdf", "*.svg"),
+        "max_size": (690, 690),
+        "output_types": ("original", "webp"),
     },
 }
 
 config.linters = [
-    'ursus.linters.footnotes.OrphanFootnotesLinter',
-    'ursus.linters.images.UnusedImagesLinter',
-    'ursus.linters.markdown.MarkdownInternalLinksLinter',
-    'ursus.linters.markdown.MarkdownLinkTextsLinter',
-    'ursus.linters.markdown.MarkdownLinkTitlesLinter',
-    'ursus.linters.markdown.RelatedEntriesLinter',
+    "ursus.linters.footnotes.OrphanFootnotesLinter",
+    "ursus.linters.images.UnusedImagesLinter",
+    "ursus.linters.markdown.MarkdownInternalLinksLinter",
+    "ursus.linters.markdown.MarkdownLinkTextsLinter",
+    "ursus.linters.markdown.MarkdownLinkTitlesLinter",
+    "ursus.linters.markdown.RelatedEntriesLinter",
 ]
 
 config.logging = {
-    'level': logging.INFO,
-    'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)d] %(message)s',
+    "level": logging.INFO,
+    "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)d] %(message)s",
 }
 
-config.add_markdown_extension('nl2br')
+config.add_markdown_extension("nl2br")
